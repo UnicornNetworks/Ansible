5f10708caa66b80721413b32ccff4c8e22cf8bac
Your Name <you@example.com>
2019-11-12 17:03:18 +0100

    lint shell code, add ansible and yaml linting
    Signed-off-by: Your Name <you@example.com>

--- ansible_run.sh
+++ ansible_run.sh
@@ -7,11 +7,23 @@ TMPHOSTS=$(mktemp)
 if [ -z "$NETWORK" ]; then
   echo "Missing network."
   exit 1
-elif [ -z "$KNOWN_HOSTS" -o ! -f "$KNOWN_HOSTS" ]; then
+elif [ -z "$KNOWN_HOSTS" ] || [ ! -f "$KNOWN_HOSTS" ]; then
   echo "Verify the KNOWN_HOSTS file."
 fi
 
-for host in $(egrep -v '#|\[' /etc/ansible/hosts | awk '{print $1}' | uniq); do
+if ! find ./ -type f -name '*.y*ml' ! -name '.*' -print0 | \
+  xargs -0 ansible-lint -x 403 -x 204; then
+    echo 'ansible-lint failed.'
+    exit 1
+fi
+
+if ! find ./ -type f -name '*.y*ml' ! -name '.*' -print0 | \
+  xargs -0 yamllint -d "{extends: default, rules: {line-length: {level: warning}}}"; then
+    echo 'yamllint failed.'
+    exit 1
+fi
+
+for host in $(grep -E -v '#|\[' /etc/ansible/hosts | awk '{print $1}' | uniq); do
   ssh-keyscan -H "$host" >> "$TMPHOSTS"
 done
 
